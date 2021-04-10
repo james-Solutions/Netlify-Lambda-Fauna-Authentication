1417371235967aeac810bb80da5728e09888701f
Sean Rowe <srowe@surgeforward.com>
2021-04-10 12:13:25 -0500

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-var webpack = require("webpack");
-/* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
-module.exports = {
-  plugins: [new webpack.DefinePlugin({ "global.GENTLY": false })],
-  node: {
-    __dirname: true,
-  },
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(proxy('/.netlify/functions/', {
+    target: 'http://localhost:9000/',
+    "pathRewrite": {
+      "^/\\.netlify/functions": ""
+    }
+  }));
 };
