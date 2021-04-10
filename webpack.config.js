8859c4c8668211b77afe9a3b869d669b17ce50e6
James <jxbromley@ualr.edu>
2021-04-09 21:28:22 -0500

    Added missing webpack files and references.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-const webpack = require('webpack');
-
+var webpack = require("webpack");
+/* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
 module.exports = {
-  plugins: [ new webpack.DefinePlugin({ "global.GENTLY": false }) ]
+  plugins: [new webpack.DefinePlugin({ "global.GENTLY": false })],
+  node: {
+    __dirname: true,
+  },
 };
