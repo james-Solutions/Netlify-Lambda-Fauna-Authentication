2b3e1d8b26489bbb9a89ac965e1b3c1b2a55a103
James <jxbromley@ualr.edu>
2021-04-20 15:52:00 -0500

    Added the creation of a one time user code for email verification in registration.

--- functions/register.js
+++ functions/register.js
@@ -14,6 +14,10 @@ const headers = {
   "Access-Control-Allow-Methods": "GET, POST, OPTION",
 };
 
+let domainUrl = process.env.REACT_APP_DEV
+  ? "https://localhost:3000"
+  : "https://modest-cori-434d1e.netlify.app";
+
 function createUser(userData) {
   return clientFauna.query(
     query.Create(query.Collection("users"), {
@@ -31,41 +35,70 @@ function createUser(userData) {
   );
 }
 
+function createUserCode(email, code) {
+  return clientFauna.query(
+    query.Create(query.Collection("users_verification_codes"), {
+      data: {
+        email,
+        code,
+      },
+    })
+  );
+}
+
 exports.handler = (event, context, callback) => {
   if (event.httpMethod === "POST") {
     const userData = JSON.parse(event.body);
     createUser(userData)
       .then((user) => {
-        console.log(`User created: ${user}`);
-        // Send Email
-        clientSparkpost.transmissions
-          .send({
-            content: {
-              from: "verification@sparkpost.studying.solutions",
-              subject: `${userData.username}, email verification for SSP Account`,
-              html: `
-              <html>
-                <body>
-                  <p>My cool email.</p>
-                </body>
-              </html>`,
-            },
-            recipients: [{ address: userData.email }],
-          })
-          .then((data) => {
-            console.log(data);
-            return callback(null, {
-              statusCode: 200,
-              headers: headers,
-              body: JSON.stringify({ message: "Successful" }),
-            });
+        const code = Math.floor(100000 + Math.random() * 900000);
+        // Create the one time code
+        createUserCode(userData.email, code)
+          .then((response) => {
+            // Send Email
+            clientSparkpost.transmissions
+              .send({
+                content: {
+                  from: "verification@sparkpost.studying.solutions",
+                  subject: `${userData.username}, email verification for SSP Account`,
+                  html: `
+                  <html>
+                    <body>
+                      <p>Thank you for signing up for the Student Scheduler Planner (SSP).</p>
+                      <p>Please <a href=${domainUrl}/${userData.email}>click me</a> to verify your email.</p>
+                      <p>Use code <span style="color:red">${code}</span> to complete your verification.</p>
+                      <p>Once your account has been verified and approved, you will be able to login.</p>
+                    </body>
+                  </html>`,
+                },
+                recipients: [{ address: userData.email }],
+              })
+              .then((data) => {
+                console.log(data);
+                return callback(null, {
+                  statusCode: 200,
+                  headers: headers,
+                  body: JSON.stringify({ message: "Successful" }),
+                });
+              })
+              .catch((error) => {
+                console.log(error);
+                return callback(null, {
+                  statusCode: 200,
+                  headers: headers,
+                  body: JSON.stringify({
+                    message: "Failure",
+                    description: error,
+                  }),
+                });
+              });
           })
           .catch((error) => {
             console.log(error);
             return callback(null, {
               statusCode: 200,
               headers: headers,
-              body: JSON.stringify({ message: "Failure" }),
+              body: JSON.stringify({ message: "Failure", description: error }),
             });
           });
       })
@@ -74,7 +107,7 @@ exports.handler = (event, context, callback) => {
         return callback(null, {
           statusCode: 400,
           headers: headers,
-          body: JSON.stringify({ message: "Failure" }),
+          body: JSON.stringify({ message: "Failure", description: e }),
         });
       });
   } else {
